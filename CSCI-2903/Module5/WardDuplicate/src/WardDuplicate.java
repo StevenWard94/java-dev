68323827a7e9835e0c4bf3ffba83feb64569c5f7
Steven Ward <stevenward94@gmail.com>
2016-09-26 19:25:20 -0500

    Add 'Module5' directory and directories (so far) for 'WardDuplicate' project.

---
+++ CSCI-2903/Module5/WardDuplicate/src/WardDuplicate.java
@@ -0,0 +1,27 @@
+/**
+ * Steven Ward
+ * WardDuplicate.java | Duplicate Project
+ *
+ * A program to validate a user-input array and output information about duplicate elements.
+ * This program first prompts the user to enter the number of elements they intend to input. This
+ * number is validated to ensure that it is greater than or equal to 15, and the program will prompt
+ * the user for another number (this cycle is repeated until a valid number is given); from now on,
+ * this number will be referred to as 'N'. Once the user has entered a legal value for N, he/she
+ * will be prompted, one at a time, for N integer elements. Each element must be an integer value on
+ * the interval, [1,N] (note: this range is inclusive), and if it is not, the user will be
+ * prompted repeatedly until an acceptable value has been input. Once the array has been populated
+ * with N valid elements, this program presents an output containing the following information:
+ *     a) the array elements given by the user (i.e. "the array")
+ *     b) the elements, if any, that were duplicated
+ *     c) if there are duplicate values, those values and the number of times they were duplicated.
+ * When the user closes this final output window, the program will terminate.
+ *
+ * Due Date: October 2, 2016
+ *
+ * @see #main(String[] args)
+ * TODO: Add 'see' tags for the methods in this program
+ *       Add any additional tags as needed
+ */
+
+public class WardDuplicate {
+}
