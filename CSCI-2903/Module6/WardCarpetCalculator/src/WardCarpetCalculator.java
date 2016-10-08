d4a57d97a57a2c2b6ee530aa50c1b59bdf97ea3c
Steven Ward <stevenward94@gmail.com>
2016-10-08 18:35:31 -0500

    Add completed WardCarpetCalculator.java - program runs perfectly with no bugs. Will finish documentation tomorrow

--- CSCI-2903/Module6/WardCarpetCalculator/src/WardCarpetCalculator.java
+++ CSCI-2903/Module6/WardCarpetCalculator/src/WardCarpetCalculator.java
@@ -6,8 +6,7 @@
  *
  * Due Date: October 09, 2016
  *
- * TODO: FINISH METHOD TO GET LENGTH AND WIDTH
- *       WRITE METHOD TO GET PRICE AND INSTANTIATE RoomCarpet OBJECT
+ * TODO:
  *       WRITE METHOD TO PERFORM CALCULATIONS AND HANDLE PROGRAM LOOP CONTROL
  *       WRITE main(String[] args) (should be only a few lines after all the other methods)
  */
@@ -37,19 +36,25 @@ import javax.swing.SwingConstants;
 public class WardCarpetCalculator {
 
   public static final String DIALOG_TITLE = "Carpet Calculator! courtesy of Westfield Carpet Company";
+  public static final String CLOSED_DIALOG = "USER CLOSED INPUT DIALOG";
   public static final int TEXT_WIDTH = 500;
 
   public static void main(String[] args) {
     showHelpDialog();
+    applicationController();
   }
 
 
-  public static void calculatorRoutine() {
+  public static void applicationController () {
+    do {
+      RoomCarpet currentCalculation = buildRoomCarpetObj( getDimensions() );
+      String currentCalcStr = currentCalculation.toString();
 
+      String resultTitle = "Result | " + DIALOG_TITLE;
+      JOptionPane.showMessageDialog(null, wrapMessage(TEXT_WIDTH, currentCalcStr),
+          resultTitle, JOptionPane.INFORMATION_MESSAGE);
+    } while( showContinuePrompt() );
   }
-
-
-  private static void getCarpetData() {}
 
 
   private static boolean tryParseDouble(String value) {
@@ -63,13 +68,57 @@ public class WardCarpetCalculator {
   }
 
 
-  private static RoomDimension getSize() {
-    // constant for 'int messageType' parameter of 'JOptionPane.showInputDialog' function.
+  private static RoomCarpet buildRoomCarpetObj(final double[] dimensions) {
     final int msg_t = JOptionPane.QUESTION_MESSAGE;
-    // constant to assist with 'showInputDialog(..)->showExitPrompt()' controller.
-    final String CLOSED_DIALOG = "USER CLOSED INPUT DIALOG";
+    // First, extract length and width values from 'double[] dimensions' parameter...
+    final double length = dimensions[0];
+    final double width = dimensions[1];
 
-    // first, get the length of the user's room...
+    // Next, get the price per sq ft of the user's carpet...
+    final String costMsg = "length:  " + length + "<br>width:  " + width + "<br><br>Please enter the "
+      + "PRICE of your new Westfield Carpets in DOLLARS PER SQUARE FOOT:";
+    final String costTitle = "Price | " + DIALOG_TITLE;
+
+    String costStr = JOptionPane.showInputDialog(null, wrapMessage(TEXT_WIDTH, costMsg)
+        , costTitle, msg_t);
+
+    if (costStr == null || "".equals(costStr)) {
+      if (showExitPrompt()) {
+        System.exit(141);
+      }
+      else {
+        costStr = CLOSED_DIALOG;
+      }
+    }
+    while (costStr.equals(CLOSED_DIALOG) || !tryParseDouble(costStr) || Double.parseDouble(costStr) <= 0.0) {
+      if (costStr.equals(CLOSED_DIALOG)) {
+        costStr = JOptionPane.showInputDialog(null, wrapMessage(TEXT_WIDTH, costMsg), costTitle, msg_t);
+      }
+      else {
+        String errmsg = "Invalid Unit Price: " + costStr + "<br><br>Please enter the PRICE, in "
+          + "DOLLARS PER SQUARE FOOT, provided to you by Westfield Carpet Company:";
+        costStr = showInvalidInputPrompt(errmsg);
+      }
+
+      if (costStr == null || "".equals(costStr)) {
+        if (showExitPrompt()) {
+          System.exit(142);
+        }
+        else {
+          costStr = CLOSED_DIALOG;
+        }
+      }
+    }
+    final double cost = Double.parseDouble(costStr);
+    return new RoomCarpet( new RoomDimension(length, width), cost );
+  }
+
+
+  private static double[] getDimensions() {
+    // Constant for 'int messageType' parameter of 'JOptionPane.showInputDialog' function.
+    final int msg_t = JOptionPane.QUESTION_MESSAGE;
+
+    // First, get the length of the user's room...
     final String lengthMsg = "length:<br>width:<br><br>Please enter the LENGTH of your room in FEET:";
     final String lengthTitle = "Length | " + DIALOG_TITLE;
 
@@ -99,17 +148,57 @@ public class WardCarpetCalculator {
 
       if (lengthStr == null || "".equals(lengthStr)) {
         if (showExitPrompt()) {
-          System.exit(121);
+          System.exit(122);
         }
         else {
           lengthStr = CLOSED_DIALOG;
         }
       }
     }
+    final double length = Double.parseDouble(lengthStr);  // Complete length retrieval.
+    // Next, get the width of the user's room...
+    final String widthMsg = "length:  " + length + "<br>width:<br><br>Please enter the WIDTH of your room in FEET:";
+    final String widthTitle = "Width | " + DIALOG_TITLE;
 
-    // TODO: FINISH CODE TO GET WIDTH
-    return new RoomDimension(1.1,1.1);  // REMOVE THIS LINE! ONLY TO QUIET ERRORS
+    String widthStr = JOptionPane.showInputDialog(null, wrapMessage(TEXT_WIDTH, widthMsg)
+        , widthTitle, msg_t);
+
+    if (widthStr == null || "".equals(widthStr)) {
+      if (showExitPrompt()) {
+        System.exit(131);
+      }
+      else {
+        widthStr = CLOSED_DIALOG;
+      }
+    }
+
+    while (widthStr.equals(CLOSED_DIALOG) || !tryParseDouble(widthStr) || Double.parseDouble(widthStr) <= 0.0) {
+      if (widthStr.equals(CLOSED_DIALOG)) {
+        widthStr = JOptionPane.showInputDialog(null, wrapMessage(TEXT_WIDTH, widthMsg)
+            , widthTitle, msg_t);
+      }
+      else {
+        String errmsg = tryParseDouble(widthStr) ?
+          "Invalid Width Measurement!<br><br>Please enter a NON-ZERO WIDTH for your room, in FEET:" :
+          ( "Invalid Width Measurement:  " + widthStr + "<br><br>Please enter a valid WIDTH for your room, in FEET:" );
+        widthStr = showInvalidInputPrompt(errmsg);
+      }
+
+      if (widthStr == null || "".equals(widthStr)) {
+        if (showExitPrompt()) {
+          System.exit(132);
+        }
+        else {
+          widthStr = CLOSED_DIALOG;
+        }
+      }
+    }
+    final double width = Double.parseDouble(widthStr); // Complete width retrieval.
+
+    return new double[] { length, width };
   }
+
+
 
 
   private static JLabel wrapMessage(int width, final String message) {
@@ -134,25 +223,43 @@ public class WardCarpetCalculator {
   }
 
 
-  private static void showErrorPrompt(final String errorMsg) {
-    final String errorTitle = "Whoops! | " + DIALOG_TITLE;
-    JOptionPane.showMessageDialog(null, wrapMessage(TEXT_WIDTH, errorMsg), errorTitle, JOptionPane.ERROR_MESSAGE);
-  }
-
   private static String showInvalidInputPrompt(final String errorMsg) {
     final String errorTitle = "Invalid Input! | " + DIALOG_TITLE;
     return JOptionPane.showInputDialog(null, wrapMessage(TEXT_WIDTH, errorMsg), errorTitle, JOptionPane.ERROR_MESSAGE);
   }
 
 
-  private static int showContinuePrompt() {
+  private static boolean showContinuePrompt() {
     final String continueMessage = "Would you like to continue using the Carpet Calculator?";
     final String continueTitle = "Continue? | " + DIALOG_TITLE;
 
     final Object[] choices = new Object[] { "Continue", "Exit", "Help" };
 
-    return JOptionPane.showOptionDialog(null, wrapMessage(TEXT_WIDTH, continueMessage), continueTitle,
+    final int choice = JOptionPane.showOptionDialog(null, wrapMessage(TEXT_WIDTH, continueMessage), continueTitle,
         JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, choices, choices[2]);
+
+    boolean onemore;
+    switch (choice) {
+      case JOptionPane.CLOSED_OPTION:
+        if (showExitPrompt()) {
+          System.exit(211);
+        }
+        onemore = showContinuePrompt();
+        break;
+      case 0:
+        onemore = true;
+        break;
+      case 1:
+        onemore = false;
+        break;
+      case 2:
+        // fall through
+      default:
+        showHelpDialog();
+        onemore = showContinuePrompt();
+        break;
+    }
+    return onemore;
   }
 
 
@@ -160,21 +267,25 @@ public class WardCarpetCalculator {
     final String exitMessage = "Are you sure you want to quit? Current calculation progress will not be saved.";
     final String exitTitle = "Exit? | " + DIALOG_TITLE;
 
-    final int options = JOptionPane.YES_NO_OPTION;
-    final Object[] labels = new Object[] { "Quit", "Return to Calculator" };
+    final int options = JOptionPane.YES_NO_CANCEL_OPTION;
+    final Object[] labels = new Object[] { "Quit", "Return to Calculator", "Help" };
 
     final int choice = JOptionPane.showOptionDialog(null, wrapMessage(TEXT_WIDTH, exitMessage), exitTitle,
-        options, JOptionPane.WARNING_MESSAGE, null, labels, labels[1]);
+        options, JOptionPane.WARNING_MESSAGE, null, labels, labels[2]);
 
     boolean confirmQuit;
     switch (choice) {
       case JOptionPane.CLOSED_OPTION:
-        //fall through
+        // fall through
       case JOptionPane.YES_OPTION:
         confirmQuit = true;
         break;
+      case JOptionPane.CANCEL_OPTION:
+        showHelpDialog();
+        confirmQuit = showExitPrompt();
+        break;
       case JOptionPane.NO_OPTION:
-        //fall through
+        // fall through
       default:
         confirmQuit = false;
         break;
