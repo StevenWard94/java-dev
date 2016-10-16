e970b142a60b6eb0a1124e8af8102cb825489c9d
Steven Ward <stevenward94@gmail.com>
2016-10-15 19:55:00 -0500

    Add method to get ship name from user

--- CSCI-2903/Module7/WardShip/src/WardShip.java
+++ CSCI-2903/Module7/WardShip/src/WardShip.java
@@ -22,7 +22,7 @@ public class WardShip {
   }
 
 
-  public static int getShipType( ) {
+  private static int getShipType( ) {
     final String title = "Choosing the Type of Ship";
     final String content = "<font size=+2>The information required for each of type of ship is listed below:</font><br>"
         + "<ul><li><font size=+1 color=blue><i>Generic Ship:</i></font><ol><li>The ship's name"
@@ -38,5 +38,15 @@ public class WardShip {
         JOptionPane.showOptionDialog( null, Utility.format(Utility.WRAP, content, SwingConstants.LEFT)
                                     , title, optionType, Utility.INFO, null, choices, choices[0] );
     return shipType;
+  }
+
+
+  private static String getName( ) {
+    final String title = "Step 1";
+    final String prompt = "1. Please enter the name of the ship:";
+    final String name =
+        JOptionPane.showInputDialog( null, Utility.format(Utility.WRAP, prompt, SwingConstants.LEFT)
+                                   , title, Utility.QUESTION );
+    return name;
   }
 }
