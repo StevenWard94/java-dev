a0112fb7ba895a4de57a4141f5aade1b563b4ddc
Steven Ward <stevenward94@gmail.com>
2016-10-24 22:45:07 -0500

    Add a couple more code listings to 'Module9/etc'

---
+++ csci-2903/Module9/etc/src/simpleWindow/SimpleWindowDemo.java
@@ -0,0 +1,20 @@
+/*
+ * Code Listing 12-3  |  SimpleWindowDemo.java
+ *     Chapter 12.2: Creating Windows
+ *     (pg. 769)
+*/
+
+package simpleWindow;
+
+/*
+ * This program creates an instance of the
+ * SimpleWindow class.
+*/
+
+public class SimpleWindowDemo
+{
+    public static void main(String[] args)
+    {
+        simpleWindow.SimpleWindow myWindow = new simpleWindow.SimpleWindow();
+    }
+}
