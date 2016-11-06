0cc3e37480f7e0d79b026f7fcc50a2ab4a03f5b6
Steven Ward <stevenward94@gmail.com>
2016-11-06 16:12:34 -0600

    Add completed first draft of 'WardCheckBoxes' class & program

--- csci-2903/Module10/WardCheckBoxes/src/WardCheckBoxes.java
+++ csci-2903/Module10/WardCheckBoxes/src/WardCheckBoxes.java
@@ -8,9 +8,21 @@
  *
  */
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
 /**
  * Defines the GUI to be used as this program's primary input/ouput interface.
@@ -28,6 +40,7 @@ import javax.swing.*;
  * application.
  *
  * @see          javax.swing.JFrame
+ * @see          java.awt.GridBagLayout
  * @see          javax.swing.JCheckBox
  * @see          java.awt.event.ItemListener
  * @see          java.awt.Color
@@ -36,6 +49,8 @@ import javax.swing.*;
 public class WardCheckBoxes extends JFrame {
 
   private static final long serialVersionUID = 0L;
+  private static final int MIN_WIN_WIDTH = 800;
+  private static final int MIN_WIN_HEIGHT = 600;
 
   private static final String DEFAULT_TITLE     = "Black on Gray";
   private static final String BLUE_TITLE        = "Yellow on Blue";
@@ -44,20 +59,22 @@ public class WardCheckBoxes extends JFrame {
 
   private JPanel checkboxPanel;
   private JCheckBox blueCheckBox, orangeCheckBox;
-  private boolean[] boxesChecked;
+  private ItemListener checkboxListener;
+  private GridBagConstraints layoutConstraints;
+  private boolean[] checkboxStatus;
 
   private class ColorChangeListener implements ItemListener {
     @Override
     public void itemStateChanged(ItemEvent event) {
       Object checkbox = event.getSource();
       if (checkbox == WardCheckBoxes.this.blueCheckBox) {
-        WardCheckBoxes.this.boxesChecked[0] = blueCheckBox.isSelected();
+        WardCheckBoxes.this.checkboxStatus[0] = blueCheckBox.isSelected();
       }
       else {
-        WardCheckBoxes.this.boxesChecked[1] = orangeCheckBox.isSelected();
+        WardCheckBoxes.this.checkboxStatus[1] = orangeCheckBox.isSelected();
       }
 
-      boolean blueChecked = boxesChecked[0], orangeChecked = boxesChecked[1];
+      boolean blueChecked = checkboxStatus[0], orangeChecked = checkboxStatus[1];
       if (blueChecked && orangeChecked) {
         WardCheckBoxes.this.checkboxPanel.setBackground(Color.BLUE);
         WardCheckBoxes.this.checkboxPanel.setForeground(Color.ORANGE);
@@ -83,5 +100,60 @@ public class WardCheckBoxes extends JFrame {
       WardCheckBoxes.this.revalidate();
       WardCheckBoxes.this.repaint();
     }
+  }
+
+
+  public static void main(String[] args) {
+    WardCheckBoxes mainWindow = new WardCheckBoxes();
+    mainWindow.setVisible(true);
+  }
+
+
+  public WardCheckBoxes( ) {
+
+    super(DEFAULT_TITLE);
+    this.setMinimumSize(new Dimension(MIN_WIN_WIDTH, MIN_WIN_HEIGHT));
+
+    this.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+    this.addWindowListener(new WindowAdapter() {
+      @Override
+      public void windowClosing(WindowEvent event) {
+        int confirmExit =
+            JOptionPane.showConfirmDialog(null, "Leaving so soon?", "Close Application?",
+                                          JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+        if (confirmExit == JOptionPane.YES_OPTION) {
+          System.exit(0);
+        }
+      }
+    });
+
+    this.setLayout(new GridBagLayout());
+    this.checkboxPanel.setLayout(new GridBagLayout());
+    checkboxPanel.setPreferredSize(this.getSize());
+    this.checkboxListener = new ColorChangeListener();
+
+    this.blueCheckBox = new JCheckBox("BLUE");
+    blueCheckBox.setFont(blueCheckBox.getFont().deriveFont(Font.BOLD, 48.0f));
+    blueCheckBox.addItemListener(checkboxListener);
+
+    this.orangeCheckBox = new JCheckBox("ORANGE");
+    orangeCheckBox.setFont(blueCheckBox.getFont());
+    orangeCheckBox.addItemListener(checkboxListener);
+    orangeCheckBox.setPreferredSize(blueCheckBox.getPreferredSize());
+
+    this.layoutConstraints = new GridBagConstraints();
+    layoutConstraints.anchor = GridBagConstraints.CENTER;
+    layoutConstraints.fill = GridBagConstraints.BOTH;
+    layoutConstraints.ipadx = 2;
+    layoutConstraints.gridx = 0;
+    layoutConstraints.gridy = 0;
+    checkboxPanel.add(blueCheckBox, layoutConstraints);
+
+    layoutConstraints.gridx = 1;
+    checkboxPanel.add(orangeCheckBox, layoutConstraints);
+
+    layoutConstraints.ipadx = 0;
+    layoutConstraints.gridx = 0;
+    this.add(checkboxPanel, layoutConstraints);
   }
 }
