9cf3ad0cc3ca29d8cd22f202f76cb909eb34190a
Steven Ward <stevenward94@gmail.com>
2016-11-24 18:15:08 -0600

    Add mostly empty 'src/MachineController.java' file to WardDrinkMachine project

---
+++ csci-2903/Module13/WardDrinkMachine/src/MachineController.java
@@ -0,0 +1,13 @@
+/**
+ * Steven Ward
+ * MachineController.java | Drink Machine Project
+ *
+ * This file contains the definition of the MachineController class, which is described below.
+ *
+ * Due Date: December 4, 2016
+ *
+ */
+
+public class MachineController {
+
+}
